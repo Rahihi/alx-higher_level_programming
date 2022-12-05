eef893180e8292a1877a78acc77b18ef121389c3
Rahel <rahelalem33@gmail.com>
2022-12-05 05:04:21 -0800

    Task 01

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-
-
 def print_list_integer(my_list=[]):
-"""
-prints a list of given integers
+"""print integer in list
+Args:
+my_list: the list to print from
+Returns:
+NULL
 """
 for i in my_list:
 print("{:d}".format(i))
